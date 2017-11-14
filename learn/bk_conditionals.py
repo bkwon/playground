11cef5db45ff794b274fa92f2e03309f91f1d389
Bryant-Kwon <bryant.kwon@autotradergroup.com>
2017-11-13 23:19:35 -0800

    Got rid of multiple declarations of same var

--- learn/bk_conditionals.py
+++ learn/bk_conditionals.py
@@ -1,5 +1,5 @@
 """
-Demonstration of else and elif.
+Demonstration Logic and Conditionals
 """
 
 def greet(friend, bling):
@@ -55,3 +55,74 @@ def am_i_hungry(hunger, cash):
 TUMMY = True
 MOOLA = 10
 am_i_hungry(TUMMY, MOOLA)
+
+
+def boolean_exp(val_a, val_b):
+    print("Value: " + str(not(val_a or not val_b)))
+
+print()
+print("Evaluate boolean logic expression for 'not(A or not B)'")
+boolean_exp(True, True)
+boolean_exp(True, False)
+boolean_exp(False, True)
+boolean_exp(False, False)
+
+print()
+print("Which expressions evaluate to true")
+print(not(True == False))
+print(True == False)
+print(not True)
+print(True != False)
+
+print()
+print("Logically Equivalent example")
+print(2 >= 1)
+print((2 > 1) or (2 == 1))
+
+
+def nand(bool1, bool2):
+    """
+    Take two Boolean values bool1 and bool2
+    and return the specified Boolean values
+    """
+
+    if bool1:
+        if bool2:
+            return False
+        else:
+            return True
+    else:
+        return True
+
+BOOL1 = True
+BOOL2 = True
+
+print()
+print("The Control Boolean Expression, nand(bool1, bool2) result: " + str(nand(BOOL1, BOOL2)))
+print("Choice #1 not(bool1 and bool2): " + str(not(BOOL1 and BOOL2)))
+print("Choice #2 (bool1 or bool2): " + str(BOOL1 or BOOL2))
+print("Choice #3: not(bool1 or bool2)" + str(not(BOOL1 or BOOL2)))
+print("Choice #4: (bool1 and bool2)" + str(BOOL1 and BOOL2))
+
+
+#Collatz Conjecture
+def f(num):
+#    print("Number is: " + str(num))
+    xval = num % 2
+#    print(xval)
+    if xval == 0:
+        result = num//2
+#        print("If result: " + str(result))
+        return result
+
+    else:
+        result = 3 * num + 1
+#        print("Else result: " + str(result))
+        return result
+
+print()
+print("Entering Collatz Exercise...")
+# f(f(f(f(f(f(f(674)))))))
+print("Collatz Result is: " + str(f(f(f(f(f(f(f(f(f(f(f(f(f(f(1071))))))))))))))))
+print()
+
